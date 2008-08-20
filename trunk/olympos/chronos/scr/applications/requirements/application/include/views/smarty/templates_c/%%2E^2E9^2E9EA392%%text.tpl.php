3fc3ef4e45d4de858d1b44ee63c5db8a85e7c27a
pgiuseppe <pgiuseppe@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2008-08-20 08:49:48 +0000

    requirements model

--- trunk/olympos/chronos/scr/applications/requirements/application/include/views/smarty/templates_c/%%2E^2E9^2E9EA392%%text.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.19, created on 2008-04-03 20:26:46
-         compiled from C:%5CProgramme%5Cxampp%5Chtdocs%5C26/wcmf/application/views/display/text.tpl */ ?>
-<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
-smarty_core_load_plugins(array('plugins' => array(array('modifier', 'default', 'C:\\Programme\\xampp\\htdocs\\26/wcmf/application/views/display/text.tpl', 1, false),array('modifier', 'truncate', 'C:\\Programme\\xampp\\htdocs\\26/wcmf/application/views/display/text.tpl', 2, false),)), $this); ?>
-<?php $this->assign('attributes', ((is_array($_tmp=@$this->_tpl_vars['attributes'])) ? $this->_run_mod_handler('default', true, $_tmp, 'class="txtdefault"') : smarty_modifier_default($_tmp, 'class="txtdefault"'))); ?>
-<span <?php echo $this->_tpl_vars['attributes']; ?>
-><?php echo ((is_array($_tmp=((is_array($_tmp=$this->_tpl_vars['value'])) ? $this->_run_mod_handler('truncate', true, $_tmp, 50, "...", true) : smarty_modifier_truncate($_tmp, 50, "...", true)))) ? $this->_run_mod_handler('default', true, $_tmp, "...") : smarty_modifier_default($_tmp, "...")); ?>
-</span>
