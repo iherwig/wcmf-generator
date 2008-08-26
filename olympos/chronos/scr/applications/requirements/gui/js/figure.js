d9840bf44359d9b45344aee56a2a3b2f8806c619
Niko Stotz <enikao@users.noreply.github.com>
2008-08-26 22:28:02 +0000

    Single Figure Class introduced

--- olympos/chronos/scr/applications/requirements/gui/js/figure.js
+++ olympos/chronos/scr/applications/requirements/gui/js/figure.js
@@ -1,3 +1,5 @@
+//Header
+
 req.figure.list = [
 	"ChiGoal",
 	"ChiRequirement",
@@ -13,22 +15,317 @@ req.figure.list = [
 ];
 
 
+//Class: ChiGoal
 
 req.figure.ChiGoal = function(label) {
 	req.figure.RectFigure.call(this, "ChiGoal", label);
 }
 
 req.figure.ChiGoal.prototype = new req.figure.RectFigure;
+
 req.figure.ChiGoal.prototype.type = "req.figure.ChiGoal";
 
+req.figure.ChiGoal.prototype.getGrid = function(store) {
+	return new Ext.grid.GridPanel({
+					columns: [{
+						header: "ChiGoal",
+						dataIndex: "Name",
+						sortable: true
+					}],
+					store: store,
+					enableDragDrop: true,
+					selModel: new Ext.grid.RowSelectionModel({
+						singleSelect: true
+					}),
+					ddGroup: "gridDDGroup",
+					reqClassName: "ChiGoal"
+				});
+}
+
+req.figure.ChiGoal.prototype.getStore = function(url){
+	return new Ext.data.Store({
+		url: url + "&type=ChiGoal",
+		reader: new Ext.data.JsonReader({
+			totalProperty: "totalCount",
+			root: "objects",
+			id: "oid",
+			fields: [{
+				name: "Name",
+				mapping: "values[1].Name.value"
+			}]
+		})
+	});
+}
+
+req.figure.ChiGoal.prototype.showEdit = function(bd){
+
+	Ext.form.Field.prototype.msgTarget = 'side';
+	
+	var form = new Ext.FormPanel({
+	
+		labelWidth: 150,
+		url: '',
+		frame: true,
+		title: 'ChiGoal Edit View',
+		bodyStyle: 'padding:5px 5px 0',
+		width: 500,
+		defaults: {
+			width: 230
+		},
+		defaultType: 'textfield',
+		items: [{
+			fieldLabel: 'id',
+			name: 'id',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'fk_chibusinessprocess_id',
+			name: 'fk_chibusinessprocess_id',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'fk_package_id',
+			name: 'fk_package_id',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'fk_chigoal_id',
+			name: 'fk_chigoal_id',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'Priority',
+			name: 'Priority',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'Value_Name',
+			name: 'Value_Name',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'Value_ammount',
+			name: 'Value_ammount',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'Value_Goal',
+			name: 'Value_Goal',
+			allowBlank: false,
+			inputType: 'textfield'
+		}],
+		buttons: [{
+			text: 'Save',
+			handler: function(){
+				form.getForm().submit({
+					url: 'main.php',
+					method: 'POST',
+					success: function(form, action){
+						alert('Success: ' + action.response.responseText);
+					},
+					failure: function(form, action){
+						alert('Failure: ' + action.failureType);
+					}
+				});
+			}
+		}, {
+			text: 'Cancel',
+			handler: function(){
+				form.getForm().reset();
+			}
+		}]
+	});
+	
+	form.render(bd);
+};
+
+req.figure.ChiGoal.prototype.getConstraints = function() {
+	return {
+		ChiGoal: {
+			label: "depends on",
+			inverse: false,
+			sourceMaxConns: -1,
+			targetMaxConns: -1
+		},
+		ChiRequirement: {
+			label: "defines",
+			inverse: true,
+			sourceMaxConns: -1,
+			targetMaxConns: 1
+		
+		}
+	};
+}
+
+//Class: ChiRequirement
 
 req.figure.ChiRequirement = function(label) {
 	req.figure.RectFigure.call(this, "ChiRequirement", label);
 }
 
 req.figure.ChiRequirement.prototype = new req.figure.RectFigure;
+
 req.figure.ChiRequirement.prototype.type = "req.figure.ChiRequirement";
 
+req.figure.ChiRequirement.prototype.getGrid = function(store) {
+	return new Ext.grid.GridPanel({
+					columns: [{
+						header: "ChiRequirement",
+						dataIndex: "Name",
+						sortable: true
+					}],
+					store: store,
+					enableDragDrop: true,
+					selModel: new Ext.grid.RowSelectionModel({
+						singleSelect: true
+					}),
+					ddGroup: "gridDDGroup",
+					reqClassName: "ChiRequirement"
+				});
+}
+
+req.figure.ChiRequirement.prototype.getStore = function(url){
+	return new Ext.data.Store({
+		url: url + "&type=ChiRequirement",
+		reader: new Ext.data.JsonReader({
+			totalProperty: "totalCount",
+			root: "objects",
+			id: "oid",
+			fields: [{
+				name: "Name",
+				mapping: "values[1].Name.value"
+			}]
+		})
+	});
+}
+
+req.figure.ChiRequirement.prototype.showEdit = function(bd){
+
+	Ext.form.Field.prototype.msgTarget = 'side';
+	
+	var form = new Ext.FormPanel({
+	
+		labelWidth: 150,
+		url: '',
+		frame: true,
+		title: 'ChiRequirement Edit View',
+		bodyStyle: 'padding:5px 5px 0',
+		width: 500,
+		defaults: {
+			width: 230
+		},
+		defaultType: 'textfield',
+		items: [{
+			fieldLabel: 'id',
+			name: 'id',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'fk_chibusinessprocess_id',
+			name: 'fk_chibusinessprocess_id',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'fk_package_id',
+			name: 'fk_package_id',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'fk_chirequirement_id',
+			name: 'fk_chirequirement_id',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'fk_chigoal_id',
+			name: 'fk_chigoal_id',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'reqType',
+			name: 'reqType',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'Priority',
+			name: 'Priority',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'Author',
+			name: 'Author',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'Proofreader',
+			name: 'Proofreader',
+			allowBlank: false,
+			inputType: 'textfield'
+		}, {
+			fieldLabel: 'Status',
+			name: 'Status',
+			allowBlank: false,
+			inputType: 'textfield'
+		}],
+		buttons: [{
+			text: 'Save',
+			handler: function(){
+				form.getForm().submit({
+					url: 'main.php',
+					method: 'POST',
+					success: function(form, action){
+						alert('Success: ' + action.response.responseText);
+					},
+					failure: function(form, action){
+						alert('Failure: ' + action.failureType);
+					}
+				});
+			}
+		}, {
+			text: 'Cancel',
+			handler: function(){
+				form.getForm().reset();
+			}
+		}]
+	});
+	
+	form.render(bd);
+};
+
+req.figure.ChiRequirement.prototype.getConstraints = function(){
+	return {
+		ChiGoal: {
+			label: "defines",
+			inverse: false,
+			sourceMaxConns: 1,
+			targetMaxConns: -1
+		
+		},
+		ChiRequirement: {
+			label: "defines",
+			inverse: false,
+			sourceMaxConns: -1,
+			targetMaxConns: -1
+		
+		},
+		ChiIssue: {
+			label: "contradicts",
+			inverse: true,
+			sourceMaxConns: -1,
+			targetMaxConns: 1
+		
+		},
+		ChiFeature: {
+			label: "implements",
+			inverse: true,
+			sourceMaxConns: -1,
+			targetMaxConns: -1
+		
+		}
+	}
+}
+
+
+//Following classes are incomplete
 
 
 req.figure.ChiFeature = function(label) {
