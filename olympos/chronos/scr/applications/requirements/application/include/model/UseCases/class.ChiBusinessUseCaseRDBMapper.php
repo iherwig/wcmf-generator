4f3dbfaab1dc5abbedc77d2915d93b34dc48f8da
Niko Stotz <enikao@users.noreply.github.com>
2008-09-04 12:04:20 +0000

    Upload application from Server

--- olympos/chronos/scr/applications/requirements/application/include/model/UseCases/class.ChiBusinessUseCaseRDBMapper.php
+++ olympos/chronos/scr/applications/requirements/application/include/model/UseCases/class.ChiBusinessUseCaseRDBMapper.php
@@ -86,7 +86,7 @@ class ChiBusinessUseCaseRDBMapper extends NodeUnifiedRDBMapper
       /* 
 	  * Value description: the main actor of this use case
 	  */
-      array('name' => 'PrimaryActor', 'app_data_type' => DATATYPE_ATTRIBUTE, 'column_name' => 'primaryactor', 'db_data_type' => 'VARCHAR(255)', 'default' => '', 'restrictions_match' => '', 'restrictions_not_match' => '', 'restrictions_description' => '', 'is_editable' => true, 'input_type' => 'select#async:Chi', 'display_type' => 'text'),
+      array('name' => 'PrimaryActor', 'app_data_type' => DATATYPE_ATTRIBUTE, 'column_name' => 'primaryactor', 'db_data_type' => 'VARCHAR(255)', 'default' => '', 'restrictions_match' => '', 'restrictions_not_match' => '', 'restrictions_description' => '', 'is_editable' => true, 'input_type' => 'select#async:ChiWorkerInternal', 'display_type' => 'text'),
       /* 
 	  * Value description: The list of actors associated with the use case. Although this information is contained in the use case itself, it helps to increase the understandability of the use case when the diagram is unavailable.
 	  */
