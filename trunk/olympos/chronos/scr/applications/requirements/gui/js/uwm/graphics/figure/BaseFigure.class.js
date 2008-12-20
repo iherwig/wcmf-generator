244063deb97e999f5534adf95e95dbe51eb83cf1
enikao <enikao@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2008-12-20 12:55:44 +0000

    Enabled context menu item "Show in grid" for figures

--- trunk/olympos/chronos/scr/applications/requirements/gui/js/uwm/graphics/figure/BaseFigure.class.js
+++ trunk/olympos/chronos/scr/applications/requirements/gui/js/uwm/graphics/figure/BaseFigure.class.js
@@ -24,41 +24,41 @@ Ext.namespace("uwm.graphics.figure");
  * @param {int} startWidth Initial width of figure.
  * @param {int} startHeight Initial height of figure.
  */
-uwm.graphics.figure.BaseFigure = function(label, figure, minWidth, minHeight, startWidth, startHeight){
-    /**
-     * Minimum width.
-     *
-     * @private
-     * @type int
-     */
-    this.minWidth = minWidth;
-    
-    /**
-     * Minimum height.
-     *
-     * @private
-     * @type int
-     */
-    this.minHeight = minHeight;
-    
-    /**
-     * The associated figure.
-     *
-     * @private
-     * @type uwm.diagram.Figure
-     */
-    this.figure = figure;
-    
-    draw2d.VectorFigure.call(this);
-    
-    if (startWidth && startHeight) {
-        this.setDimension(startWidth, startHeight);
-    }
-    if (label) {
-        this.setLabel(label);
-    }
-    
-    this.buildContextMenu();
+uwm.graphics.figure.BaseFigure = function(label, figure, minWidth, minHeight, startWidth, startHeight) {
+	/**
+	 * Minimum width.
+	 *
+	 * @private
+	 * @type int
+	 */
+	this.minWidth = minWidth;
+	
+	/**
+	 * Minimum height.
+	 *
+	 * @private
+	 * @type int
+	 */
+	this.minHeight = minHeight;
+	
+	/**
+	 * The associated figure.
+	 *
+	 * @private
+	 * @type uwm.diagram.Figure
+	 */
+	this.figure = figure;
+	
+	draw2d.VectorFigure.call(this);
+	
+	if (startWidth && startHeight) {
+		this.setDimension(startWidth, startHeight);
+	}
+	if (label) {
+		this.setLabel(label);
+	}
+	
+	this.buildContextMenu();
 }
 
 Ext.extend(uwm.graphics.figure.BaseFigure, draw2d.VectorFigure);
@@ -69,20 +69,20 @@ Ext.extend(uwm.graphics.figure.BaseFigure, draw2d.VectorFigure);
  * @private
  * @param {uwm.diagram.UwmWorkflow} workflow The workflow containing this figure.
  */
-uwm.graphics.figure.BaseFigure.prototype.setWorkflow = function(workflow){
-    draw2d.VectorFigure.prototype.setWorkflow.call(this, workflow);
-    
-    if (workflow != null && this.port == null) {
-        /**
-         * The port of this figure.
-         *
-         * @private
-         * @type uwm.graphics.connection.UwmPort
-         */
-        this.port = new uwm.graphics.connection.UwmPort();
-        this.port.setWorkflow(workflow);
-        this.addPort(this.port, this.width + 8, 0);
-    }
+uwm.graphics.figure.BaseFigure.prototype.setWorkflow = function(workflow) {
+	draw2d.VectorFigure.prototype.setWorkflow.call(this, workflow);
+	
+	if (workflow != null && this.port == null) {
+		/**
+		 * The port of this figure.
+		 *
+		 * @private
+		 * @type uwm.graphics.connection.UwmPort
+		 */
+		this.port = new uwm.graphics.connection.UwmPort();
+		this.port.setWorkflow(workflow);
+		this.addPort(this.port, this.width + 8, 0);
+	}
 }
 
 /**
@@ -90,43 +90,44 @@ uwm.graphics.figure.BaseFigure.prototype.setWorkflow = function(workflow){
  *
  * @private
  */
-uwm.graphics.figure.BaseFigure.prototype.buildContextMenu = function(){
-    var figure = this.getFigure();
-    
-    /**
-     * The context menu of this figure.
-     *
-     * @private
-     * @type Ext.menu.Menu
-     */
-    this.uwmContextMenu = new Ext.menu.Menu({
-        items: [new Ext.menu.Item({
-            text: "Show in tree",
-            handler: function(item, e){
-                figure.showInModelTree();
-            }
-        }), new Ext.menu.Item({
-            text: "Show in grid",
-            handler: function(item, e){
-                figure.showInGrid();
-            }
-        }), new Ext.menu.Item({
-            text: "Show in Hierarchy",
-            handler: function(item, e){
-                figure.showInHierarchy();
-            }
-        }), "-", new Ext.menu.Item({
-            text: "Delete from diagram",
-            handler: function(item, e){
-                figure.deleteFromDiagram();
-            }
-        }), new Ext.menu.Item({
-            text: "Delete from model",
-            handler: function(tiem, e){
-                figure.deleteFromModel();
-            }
-        })]
-    });
+uwm.graphics.figure.BaseFigure.prototype.buildContextMenu = function() {
+	var figure = this.getFigure();
+	
+	/**
+	 * The context menu of this figure.
+	 *
+	 * @private
+	 * @type Ext.menu.Menu
+	 */
+	this.uwmContextMenu = new Ext.menu.Menu({
+		items: [new Ext.menu.Item({
+			text: "Show in tree",
+			handler: function(item, e) {
+				figure.showInModelTree();
+			}
+		}), new Ext.menu.Item({
+			id: uwm.graphics.figure.BaseFigure.CONTEXTMENU_SHOW_IN_GRID_ID,
+			text: "Show in grid",
+			handler: function(item, e) {
+				figure.showInGrid();
+			}
+		}), new Ext.menu.Item({
+			text: "Show in Hierarchy",
+			handler: function(item, e) {
+				figure.showInHierarchy();
+			}
+		}), "-", new Ext.menu.Item({
+			text: "Delete from diagram",
+			handler: function(item, e) {
+				figure.deleteFromDiagram();
+			}
+		}), new Ext.menu.Item({
+			text: "Delete from model",
+			handler: function(tiem, e) {
+				figure.deleteFromModel();
+			}
+		})]
+	});
 }
 
 /**
@@ -136,8 +137,13 @@ uwm.graphics.figure.BaseFigure.prototype.buildContextMenu = function(){
  * @param {int} x X position where to show the context menu.
  * @param {int} y Y position where to show the context menu.
  */
-uwm.graphics.figure.BaseFigure.prototype.onContextMenu = function(x, y){
-    this.uwmContextMenu.showAt(this.figure.getDiagram().getContextMenuPosition(x, y));
+uwm.graphics.figure.BaseFigure.prototype.onContextMenu = function(x, y) {
+	var showInGrid = this.uwmContextMenu.items.get(uwm.graphics.figure.BaseFigure.CONTEXTMENU_SHOW_IN_GRID_ID);
+	
+	var isGridAvailable = this.figure.gridAvailable();
+	showInGrid.setDisabled(!isGridAvailable);
+	
+	this.uwmContextMenu.showAt(this.figure.getDiagram().getContextMenuPosition(x, y));
 }
 
 /**
@@ -146,8 +152,8 @@ uwm.graphics.figure.BaseFigure.prototype.onContextMenu = function(x, y){
  * @return The associated figure.
  * @type uwm.diagram.Figure
  */
-uwm.graphics.figure.BaseFigure.prototype.getFigure = function(){
-    return this.figure;
+uwm.graphics.figure.BaseFigure.prototype.getFigure = function() {
+	return this.figure;
 }
 
 /**
@@ -156,13 +162,13 @@ uwm.graphics.figure.BaseFigure.prototype.getFigure = function(){
  * @return The minimum width of this figure.
  * @type int
  */
-uwm.graphics.figure.BaseFigure.prototype.getMinWidth = function(){
-    var result = draw2d.VectorFigure.prototype.getMinWidth.call(this);
-    
-    if (this.minWidth) {
-        result = this.minWidth;
-    }
-    return result;
+uwm.graphics.figure.BaseFigure.prototype.getMinWidth = function() {
+	var result = draw2d.VectorFigure.prototype.getMinWidth.call(this);
+	
+	if (this.minWidth) {
+		result = this.minWidth;
+	}
+	return result;
 }
 
 /**
@@ -171,14 +177,14 @@ uwm.graphics.figure.BaseFigure.prototype.getMinWidth = function(){
  * @return The minimum height of this figure.
  * @type int
  */
-uwm.graphics.figure.BaseFigure.prototype.getMinHeight = function(){
-    var result = draw2d.VectorFigure.prototype.getMinHeight.call(this);
-    
-    if (this.minHeight) {
-        result = this.minHeight;
-    }
-    
-    return result;
+uwm.graphics.figure.BaseFigure.prototype.getMinHeight = function() {
+	var result = draw2d.VectorFigure.prototype.getMinHeight.call(this);
+	
+	if (this.minHeight) {
+		result = this.minHeight;
+	}
+	
+	return result;
 }
 
 /**
@@ -188,12 +194,12 @@ uwm.graphics.figure.BaseFigure.prototype.getMinHeight = function(){
  * @param {int} width New width of the figure.
  * @param {int} height New height of the figure.
  */
-uwm.graphics.figure.BaseFigure.prototype.setDimension = function(width, height){
-    draw2d.VectorFigure.prototype.setDimension.call(this, width, height);
-    
-    if (this.port != null) {
-        this.port.setPosition(this.getWidth() + 8, 0);
-    }
+uwm.graphics.figure.BaseFigure.prototype.setDimension = function(width, height) {
+	draw2d.VectorFigure.prototype.setDimension.call(this, width, height);
+	
+	if (this.port != null) {
+		this.port.setPosition(this.getWidth() + 8, 0);
+	}
 }
 
 /**
@@ -201,13 +207,13 @@ uwm.graphics.figure.BaseFigure.prototype.setDimension = function(width, height){
  *
  * @private
  */
-uwm.graphics.figure.BaseFigure.prototype.createHTMLElement = function(){
-    var item = draw2d.Figure.prototype.createHTMLElement.call(this);
-    item.className = "uwmFigure";
-    
-    uwm.Util.setElementUnselectable(item);
-    
-    return item;
+uwm.graphics.figure.BaseFigure.prototype.createHTMLElement = function() {
+	var item = draw2d.Figure.prototype.createHTMLElement.call(this);
+	item.className = "uwmFigure";
+	
+	uwm.Util.setElementUnselectable(item);
+	
+	return item;
 }
 
 /**
@@ -215,10 +221,10 @@ uwm.graphics.figure.BaseFigure.prototype.createHTMLElement = function(){
  *
  * @param {String} newText The new label text.
  */
-uwm.graphics.figure.BaseFigure.prototype.setLabel = function(newText){
-    if (this.label) {
-        this.label.innerHTML = newText;
-    }
+uwm.graphics.figure.BaseFigure.prototype.setLabel = function(newText) {
+	if (this.label) {
+		this.label.innerHTML = newText;
+	}
 }
 
 /**
@@ -227,8 +233,10 @@ uwm.graphics.figure.BaseFigure.prototype.setLabel = function(newText){
  * @return The label text.
  * @type String
  */
-uwm.graphics.figure.BaseFigure.prototype.getLabel = function(){
-    if (this.label) {
-        return this.label.innerHTML;
-    }
+uwm.graphics.figure.BaseFigure.prototype.getLabel = function() {
+	if (this.label) {
+		return this.label.innerHTML;
+	}
 }
+
+uwm.graphics.figure.BaseFigure.CONTEXTMENU_SHOW_IN_GRID_ID = "showInGrid";
