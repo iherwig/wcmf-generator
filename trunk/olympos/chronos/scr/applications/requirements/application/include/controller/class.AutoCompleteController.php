da7a14d0a046d962309187f0bc971df6d7deb27f
shentzel <shentzel@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2009-02-02 18:38:40 +0000

    Autocomplete any Types

--- trunk/olympos/chronos/scr/applications/requirements/application/include/controller/class.AutoCompleteController.php
+++ trunk/olympos/chronos/scr/applications/requirements/application/include/controller/class.AutoCompleteController.php
@@ -24,32 +24,25 @@ class AutoCompleteController extends Controller
 			$searchlimitend = $_REQUEST["limit_end"];
 			$searchlimitstart = $_REQUEST["limit_start"];
 			/*$domObj =  $_REQUEST["domobj"];*/
-			$domObj = array ('ChiBusinessUseCase', 'ChiWorkerExternal');
+			$domObj = array ('ChiBusinessUseCase', 'ChiWorkerExternal', 'ChiRequirement');
+			$datastore = array ();
 		
 			//	create and execute query
 			$persistenceFacade = & PersistenceFacade::getInstance(); // get the persistence information (database)
-		
-			$usecaseQuery = & PersistenceFacade::createObjectQuery($domObj[0]);
-			$usecaseTpl = & $usecaseQuery->getObjectTemplate('ChiBusinessUseCase');
-			$usecaseTpl->setValue("Name", $searchstring, DATATYPE_ATTRIBUTE);
-			$pagingInfo = new PagingInfo($searchlimitend);
-			$pagingInfo->setIndex($searchlimitstart);
-			$usecaselist = $usecaseQuery->execute(BUILDDEPTH_SINGLE, null, $pagingInfo);
-		
-			$workerQuery = & PersistenceFacade::createObjectQuery($domObj[1]);
-			$workerTpl = & $workerQuery->getObjectTemplate($domObj[1]);
-			$workerTpl->setValue("Name", $searchstring, DATATYPE_ATTRIBUTE);
-			$pagingInfo = new PagingInfo($searchlimitend);
-			$pagingInfo->setIndex($searchlimitstart);
-			$workerlist = $workerQuery->execute(BUILDDEPTH_SINGLE, null, $pagingInfo);
-		
-			//	Create and Fill Datastore
-			$datastore = array ();
-			foreach ($usecaselist as $key=>$val)
-			/*array_push($datastore, array ('key'=>$key, 'val'=>$val->getName()));*/
-			array_push($datastore, array ('type'=>$val->getType(), 'val'=>$val->getName()));
-			foreach ($workerlist as $key=>$val)
-			array_push($datastore, array ('type'=>$val->getType(), 'val'=>$val->getName()));
+			
+			$objlist = array();
+			foreach ($domObj as $key=>$val){
+				$objQuery = &$persistenceFacade->createObjectQuery($val);
+				$objTpl = & $objQuery->getObjectTemplate($val);
+				$objTpl->setValue("Name", $searchstring, DATATYPE_ATTRIBUTE);
+				$pagingInfo = new PagingInfo($searchlimitend);
+				$pagingInfo->setIndex($searchlimitstart);
+				$objlistpart = $objQuery->execute(BUILDDEPTH_SINGLE, null, $pagingInfo);
+
+				//	Fill Datastore
+				foreach ($objlistpart as $key=>$val)
+				array_push($datastore, array ('type'=>$val->getType(), 'val'=>$val->getName()));
+			}
 		
 			//	Set the next action
 			$this->_response->setAction('ok');
