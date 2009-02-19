2f6009e46da1eb505e9764fc35382e3711ad928d
shentzel <shentzel@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2009-02-19 15:16:44 +0000

    Object History

---
+++ trunk/olympos/chronos/scr/applications/requirements/application/include/controller/class.ObjectHistoryRestoreController.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once(BASE."wcmf/lib/presentation/class.Controller.php");
+
+class ObjectHistoryRestoreController extends Controller
+{
+	
+	function hasView()
+	{
+		return true;
+	}
+	function executeKernel()
+	{
+		
+		
+		
+		
+		return false;
+	}
+}
+?>
