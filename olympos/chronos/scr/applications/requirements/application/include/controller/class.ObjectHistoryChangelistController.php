14d4571788e5cac2a6fc2d3c628065c4f7c03b26
shentzel <shentzel@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2009-02-23 16:19:47 +0000

    ObjectHistory unserialized

--- olympos/chronos/scr/applications/requirements/application/include/controller/class.ObjectHistoryChangelistController.php
+++ olympos/chronos/scr/applications/requirements/application/include/controller/class.ObjectHistoryChangelistController.php
@@ -1,40 +1,85 @@
 <?php
 
-require_once(BASE."wcmf/lib/presentation/class.Controller.php");
+require_once (BASE."wcmf/lib/presentation/class.Controller.php");
 require_once (BASE."wcmf/lib/persistence/class.PersistenceFacade.php");
 require_once (BASE."wcmf/lib/persistence/class.ObjectQuery.php");
 
 class ObjectHistoryChangelistController extends Controller
 {
-		
+
 	function hasView()
 	{
 		return true;
 	}
+
 	function executeKernel()
 	{
 		// execute this code only if the action is histlist
 		if ($this->_request->getAction() == 'histlist') {
-		
-		//get history table as objectlist
-		$tablename = 'History' ;
-		$objlist = array();
-		$searchlimit = 30 ; 
-		$orderby = 'timestamp';
-		
-		$persistenceFacade = & PersistenceFacade::getInstance(); 
-		$objQuery = &$persistenceFacade->createObjectQuery($tablename);
-		$pagingInfo = new PagingInfo($searchlimit);
-		$objlist = $objQuery->execute(BUILDDEPTH_SINGLE, $orderby, $pagingInfo);
+					
+			 //get history table as objectlist
+			 $affectedoid = $this->_request->getValue('oid'); // 'ChiBusinessUseCase:15475'
+			 $tablename = 'History';
+			 $objlist = array ();
+			 $orderby = array('timestamp DESC');
+			 $searchlimit = 5000;
 
-		//	Set the next action
-		$this->_response->setAction('ok');
+			 // history table
+			 $persistenceFacade = & PersistenceFacade::getInstance();
+			 $objQuery = & $persistenceFacade->createObjectQuery($tablename);
+			 // only for object oid
+			 $objTpl = & $objQuery->getObjectTemplate($tablename);
+			 $objTpl->setValue("affectedoid", $affectedoid, DATATYPE_ATTRIBUTE);
+			 // searchlimit if needed
+			 $pagingInfo = new PagingInfo($searchlimit);
+			 //$objlist = $objQuery->execute(BUILDDEPTH_SINGLE, $orderby, $pagingInfo);
+			 // search
+			 $objlist = $objQuery->execute(BUILDDEPTH_SINGLE, $orderby);
+			 
+			 // write new array with unserialized data tab
+			 $objlistuns = self::getDataValue($objlist);
+			 
+			 //	Set the next action
+			 $this->_response->setAction('ok');
+			 
+			 //	Response
+			 $this->_response->setValue('changelist', $objlistuns );
+			 return false;
 
-		//	Response
-		$this->_response->setValue('changelist', $objlist );
-		return false;
-		
 		}
 	}
+
+	private function getDataValue($objlist) {
+	
+		$datatab = $objlist;
+		$arrdatatabval = array ();
+		foreach ($objlist as $key=>$val) {
+			$datatab = & $val;
+			foreach ($datatab as $k=>$v) {
+				
+				if ($k == '_data') {
+				
+					$datatabval = unserialize($v[1]['data']['value']);
+					array_push($arrdatatabval,
+					array (
+					'id'=>$v[3]['id']['value'],
+					'data'=>$datatabval[0],
+					'duplicate'=>$v[1]['duplicate']['value'],
+					'eventtype'=>$v[1]['eventtype']['value'],
+					'affectedoid'=>$v[1]['affectedoid']['value'],
+					'otherid'=>$v[1]['otherid']['value'],
+					'timestamp'=>$v[1]['timestamp']['value'],
+					'user'=>$v[1]['user']['value']
+					)
+					);
+				}
+			
+			}
+		}
+	
+		return $arrdatatabval;
+	}
+
 }
+
 ?>
