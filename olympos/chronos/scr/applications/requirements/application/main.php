e5dacefa485fbf9b2780472f7d00d3b0b7539eb2
Niko Stotz <enikao@users.noreply.github.com>
2009-02-26 17:22:03 +0000

    Fixes for MultipleActionController transferred from WCMF blank project

--- olympos/chronos/scr/applications/requirements/application/main.php
+++ olympos/chronos/scr/applications/requirements/application/main.php
@@ -1,27 +1,25 @@
 <?php
-/** 
+/**
  * wCMF - wemove Content Management Framework
- * Copyright (C) 2005 wemove digital solutions GmbH
+ * Copyright (C) 2005-2009 wemove digital solutions GmbH
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Licensed under the terms of any of the following licenses 
+ * at your choice:
  *
- * $Id: main.php 747 2008-09-01 21:50:23Z iherwig $
+ * - GNU Lesser General Public License (LGPL)
+ *   http://www.gnu.org/licenses/lgpl.html
+ * - Eclipse Public License (EPL)
+ *   http://www.eclipse.org/org/documents/epl-v10.php
+ *
+ * See the license.txt file distributed with this work for 
+ * additional information.
+ *
+ * $Id$
  */
-error_reporting(E_ALL);
+error_reporting(E_ERROR | E_PARSE);
 
 require_once("base_dir.php");  
+require_once(BASE."wcmf/lib/util/class.Log.php");
 require_once(BASE."wcmf/lib/util/class.Message.php");
 require_once(BASE."wcmf/lib/presentation/class.Request.php");
 require_once(BASE."wcmf/lib/presentation/class.Application.php");
@@ -32,7 +30,7 @@ $application = &Application::getInstance();
 $callParams = &$application->initialize();
 
 // set the error handler
-$GLOBALS['MESSAGE_ERROR_HANDLER'] = "onError";
+$GLOBALS['EXCEPTION_HANDLER'] = "onError";
 
 // process the requested action (we don't use the result here)
 $request = new Request(
@@ -41,13 +39,14 @@ $request = new Request(
   $callParams['action'], 
   $callParams['data']
 );
+$request->setFormat($callParams['requestFormat']);
 $request->setResponseFormat($callParams['responseFormat']);
 $result = ActionMapper::processAction($request);
 exit;
 
 /**
- * Global error handling function. Assigned to MESSAGE_ERROR_HANDLER 
- * which means it is called by Message::error()
+ * Global error handling function. Assigned to EXCEPTION_HANDLER 
+ * which means it is called by WCMFException::throwEx()
  * @param message The error message
  * @param file The php file in which the error occured (optional)
  * @param line The line in the php file in which the error occured (optional)
@@ -55,11 +54,11 @@ exit;
  */
 function onError($message, $file='', $line='') 
 { 
-  global $controller, $context, $action, $data, $responseFormat, $MESSAGE_LOG_FILE;
+  global $controller, $context, $action, $data, $responseFormat;
   static $numCalled = 0;
   
   $data['errorMsg'] = $message;
-  Message::log($message."\n".Message::getStackTrace(), $file, $line);
+  Log::error($message."\n".WCMFException::getStackTrace(), 'main');
   
   // rollback current transaction
   $persistenceFacade = &PersistenceFacade::getInstance();
@@ -76,11 +75,11 @@ function onError($message, $file='', $line='')
   else if ($numCalled == 3)
   {
     // make sure that no error can happen in this stage
-    $msg = 'FATAL. See \''.$MESSAGE_LOG_FILE.'\' for details.';
+    $msg = 'See log for details.';
     if ($responseFormat == MSG_FORMAT_JSON)
       print JSONUtil::encode(array('success' => false, 'errorMsg' => $msg));
     else
-      Message::hint($msg);
+      Log::fatal($msg, 'main');
     exit;
   }
   else
