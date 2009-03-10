9da1644248bee77cdd5067e42f348a93801ccd83
meilers <meilers@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2009-03-10 16:03:25 +0000

    Property view is closed when the object it shows is deleted

--- trunk/olympos/chronos/scr/applications/requirements/gui/js/uwm/property/PropertyContainer.class.js
+++ trunk/olympos/chronos/scr/applications/requirements/gui/js/uwm/property/PropertyContainer.class.js
@@ -14,7 +14,7 @@ Ext.namespace("uwm.property");
 /**
  * Container managing display/removal of properties as well as locking/unlocking
  * of shown ModelNodes.
- * 
+ *
  * @extends Ext.Panel
  * @constructor
  * @param {Object}
@@ -24,38 +24,46 @@ uwm.property.PropertyContainer = function() {
 }
 
 uwm.property.PropertyContainer = Ext.extend(Ext.Panel, {
-	initComponent : function() {
+	initComponent: function() {
 		Ext.apply(this, {
-			region :"center",
-			layout :"fit",
-			collapsible :false,
-			split :false,
-			width :250,
-			autoScroll :true,
-			title :uwm.Dict.translate('Properties')
+			region: "center",
+			layout: "fit",
+			collapsible: false,
+			split: false,
+			width: 250,
+			autoScroll: true,
+			title: uwm.Dict.translate('Properties')
 		})
-
+		
+		var self = this;
+		
 		uwm.property.PropertyContainer.instance = this;
-
-		uwm.property.PropertyContainer.superclass.initComponent.apply(this,
-				arguments);
-
+		
+		uwm.property.PropertyContainer.superclass.initComponent.apply(this, arguments);
+		
 		this.currentOid = null;
 		this.isLockedByOtherUser = null;
-
+		
 		this.on("afterlayout", this.showInfoMask);
+		
+		uwm.event.EventBroker.getInstance().addListener({
+			"delete": function(modelObject) {
+				self.handleDeleteEvent(modelObject);
+			}
+		});
 	}
 })
 
 uwm.property.PropertyContainer.prototype.showInfoMask = function() {
-	this.mask = new uwm.ui.InfoMask(
-			this.body,
-			{
-				msg :uwm.Dict
-						.translate('This panel shows the properties of each object selected by a single click.')
-			});
+	while (this.items && this.items.getCount() > 0) {
+		this.remove(this.items.get(0), true);
+	}
+	
+	this.mask = new uwm.ui.InfoMask(this.body, {
+		msg: uwm.Dict.translate('This panel shows the properties of each object selected by a single click.')
+	});
 	this.mask.show();
-
+	
 	this.un("afterlayout", this.showInfoMask);
 }
 
@@ -65,42 +73,41 @@ uwm.property.PropertyContainer.prototype.showProperty = function(modelNode) {
 		
 		if (!eastPanel.isCollapsed()) {
 			var oid = modelNode.getOid();
-
+			
 			if (oid != null && this.currentOid != oid) {
 				if (this.mask) {
 					this.mask.hide();
 				}
-
+				
 				var oldOid = this.currentOid;
-
+				
 				this.currentOid = modelNode.getOid();
-
+				
 				var items = this.items;
 				while (items && items.getCount() > 0) {
 					this.remove(items.get(0), true);
 				}
-
+				
 				var self = this;
-
+				
 				this.mask = new Ext.LoadMask(this.getEl());
 				this.mask.show();
-
+				
 				var actionSet = new uwm.persistency.ActionSet();
-
+				
 				if (oldOid) {
 					actionSet.addUnlock(oldOid);
 				}
-
+				
 				actionSet.addLock(this.currentOid, function(request, data) {
 					self.setLocked(false);
 				}, function(request, data) {
 					self.setLocked(true);
 				});
-
-				uwm.model.ModelContainer.getInstance().loadByOid(
-						this.currentOid, actionSet);
-
-				actionSet.commit( function() {
+				
+				uwm.model.ModelContainer.getInstance().loadByOid(this.currentOid, actionSet);
+				
+				actionSet.commit(function() {
 					self.displayForm();
 				});
 			}
@@ -114,20 +121,27 @@ uwm.property.PropertyContainer.prototype.setLocked = function(isLocked) {
 
 uwm.property.PropertyContainer.prototype.displayForm = function() {
 
-	var modelNode = uwm.model.ModelContainer.getInstance().getByOid(
-			this.currentOid);
-
-	var form = this.add(modelNode.getModelNodeClass().getPropertyForm(
-			modelNode, this.isLockedByOtherUser));
-
+	var modelNode = uwm.model.ModelContainer.getInstance().getByOid(this.currentOid);
+	
+	var form = this.add(modelNode.getModelNodeClass().getPropertyForm(modelNode, this.isLockedByOtherUser));
+	
 	this.doLayout();
-
+	
 	modelNode.populatePropertyForm(form);
-
+	
 	this.mask.hide();
 }
 
+uwm.property.PropertyContainer.prototype.getCurrentOid = function() {
+	return this.currentOid;
+}
+
+uwm.property.PropertyContainer.prototype.handleDeleteEvent = function(modelObject){
+	if (this.currentOid == modelObject.getOid()) {
+		this.showInfoMask();
+	}
+}
+
 uwm.property.PropertyContainer.getInstance = function() {
-	return uwm.Uwm.getInstance().getActiveWorkbench().getEastPanel()
-			.getPropertyContainer();
+	return uwm.Uwm.getInstance().getActiveWorkbench().getEastPanel().getPropertyContainer();
 }
