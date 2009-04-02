ec1bd9d361195676f15a94e663b52b5cdb4eb392
enikao <enikao@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2009-04-02 14:21:59 +0000

    Added Generator to build

--- trunk/olympos/chronos/scr/applications/requirements/Build/Folders.bat
+++ trunk/olympos/chronos/scr/applications/requirements/Build/Folders.bat
@@ -1,2 +1,5 @@
 set TORTOISE="C:\Program Files\TortoiseSVN"
 set SUBVERSION="C:\Program Files\CollabNet Subversion Client"
+REM ANT_HOME and JAVA_HOME _must_ be written without quotes
+set ANT_HOME=c:\Program Files\Java\ant
+set JAVA_HOME=c:\Program Files\Java\jdk1.6.0_12
