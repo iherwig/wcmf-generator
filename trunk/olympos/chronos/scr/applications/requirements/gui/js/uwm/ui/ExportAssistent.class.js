1384c4559124eae1396cb7e1829424b60148d57f
shentzel <shentzel@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2009-04-07 09:10:33 +0000

    ExportAssistent for document Export data binding

--- trunk/olympos/chronos/scr/applications/requirements/gui/js/uwm/ui/ExportAssistent.class.js
+++ trunk/olympos/chronos/scr/applications/requirements/gui/js/uwm/ui/ExportAssistent.class.js
@@ -18,18 +18,127 @@ Ext.namespace("uwm.ui");
  * @constructor
  * @param object The object for which the export shall be shown.
  */
+uwm.ui.ExportAssistent = function() {
 
-uwm.ui.ExportAssistent = function(object) {
-	uwm.ui.ExportAssistent.superclass.constructor.call(this, Ext.apply(this, {
-		height: 350,
-		width: 550,
-		title: uwm.Dict.translate('Export Assistant'),
-}));
+	uwm.ui.ExportAssistent.superclass.constructor.call(this, Ext.apply(this, this.getWindowLayout(this.setSelMode(this.getGrid(this.getStore()), this.getTemplate()), this.getDetailPanelItem())));
 	
+	this.addButtons();
+	this.setVisible(true);
+	
+}
+Ext.extend(uwm.ui.ExportAssistent, Ext.Window);
+
+uwm.ui.ExportAssistent.prototype.getTemplate = function() {
+
+	return new Ext.Template(['<b><u>Template Name: </b></u><BR/><BR/><center>{templateName}</center><BR/>', '<b><u>Description: </b></u><BR/><BR/><center>{description}</center><BR/>'])
+	
+}
+
+uwm.ui.ExportAssistent.prototype.setSelMode = function(grid, Tpl) {
+
+	grid.getSelectionModel().on('rowselect', function(sm, rowIdx, r) {
+		var detailPanel = Ext.getCmp('detailPanel');
+		Tpl.overwrite(detailPanel.body, r.data);
+	});
+	return grid;
+}
+
+uwm.ui.ExportAssistent.prototype.getWindowLayout = function(grid, detailPanelItem) {
+
+	return {
+		title: uwm.Dict.translate('Export Assistant'),
+		layout: 'column',
+		region: 'center',
+		height: 330,
+		width: 550,
+		items: [grid, detailPanelItem]
+	}
+	
+}
+
+uwm.ui.ExportAssistent.prototype.getDetailPanelItem = function() {
+
+	return {
+		id: 'detailPanel',
+		title: uwm.Dict.translate('detailed description'),
+		html: '<i>' + uwm.Dict.translate('< detailed description >') + '</i>',
+		split: true,
+		region: 'east',
+		height: 170,
+		width: 280,
+		frame: true,
+		bodyStyle: {
+			padding: '7px'
+		}
+	}
+	
+}
+
+uwm.ui.ExportAssistent.prototype.getStore = function() {
+
+	return new Ext.data.SimpleStore({
+		data: [{
+			technName: 'test',
+			templateName: 'testest1',
+			description: 'testestest1'
+		}, {
+			technName: 'test',
+			templateName: 'testest2',
+			description: 'testestest2'
+		}, {
+			technName: 'test',
+			templateName: 'testest3',
+			description: 'testestest3'
+		}],
+		fields: [{
+			name: 'technName',
+			mapping: 'technName'
+		}, {
+			name: 'templateName',
+			mapping: 'templateName'
+		}, {
+			name: 'description',
+			mapping: 'description'
+		}]
+	});
+	
+}
+
+uwm.ui.ExportAssistent.prototype.getGrid = function(store) {
+
+	return new Ext.grid.GridPanel({
+		store: store,
+		columns: [{
+			header: uwm.Dict.translate('Technical Name'),
+			width: 233,
+			dataIndex: 'technName',
+			sortable: true
+		}//, 
+		//{ header: uwm.Dict.translate('Template Name'), width: 233, dataIndex: 'templateName', sortable: true }, 
+		//{ header: uwm.Dict.translate('Description'), width: 233, dataIndex: 'description', sortable: true }
+		],
+		sm: new Ext.grid.RowSelectionModel({
+			singleSelect: true
+		}),
+		title: uwm.Dict.translate('Please select to show Details.'),
+		split: true,
+		region: 'west',
+		height: 250,
+		width: 250,
+		frame: true
+	
+	});
+	
+}
+
+uwm.ui.ExportAssistent.prototype.addButtons = function() {
+
 	this.addButton(new Ext.Button({
 		window: this,
 		text: uwm.Dict.translate('Export'),
-		handler: function(){alert('function under construction');}
+		handler: function() {
+			alert('function under construction');
+		}
 	}));
 	this.addButton(new Ext.Button({
 		window: this,
@@ -38,11 +147,8 @@ uwm.ui.ExportAssistent = function(object) {
 			this.window.close();
 		}
 	}));
-
-	this.setVisible(true);
 	
 }
-Ext.extend(uwm.ui.ExportAssistent, Ext.Window);
 
 uwm.ui.ExportAssistent.prototype.restoreError = function(options, data, errorMsg, callback, scope, arg) {
 	this.fireEvent("loadexception", this, options, data);
