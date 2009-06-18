2fd4cb5fc226cf78633c0213eeaab80d738afd22
shentzel <shentzel@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2009-06-18 20:51:00 +0000

    Multiselect First saving state ... works starting Multiselect.html

---
+++ olympos/chronos/scr/applications/requirements/gui/js/uwm/property/ComboboxMult.js
@@ -0,0 +1,54 @@
+
+Ext.namespace("uwm.property");
+
+uwm.property.ComboboxMult = function(config){
+		uwm.property.ComboboxMult.superclass.constructor.call(this, Ext.apply(this, {
+		labelWidth: 75, 
+		autoWidth : true 
+		}, config));
+	
+	this.toolTipText = config.toolTip
+	this.modelNode = config.modelNode;
+	
+	Ext.QuickTips.init();
+	Ext.form.Field.prototype.msgTarget = 'side';
+	
+	var multiselect = new Ext.ux.Multiselect({
+		enableToolbar	  :  true,
+		name              :  'multiselect',
+//		fieldLabel        :  config.fieldLabel+' Multiselect',
+		dataFields        :  ['code', 'desc'], 
+		data              :  [['1', 'One'], ['2', 'Two'], ['3', 'Three'], ['4', 'Four'], ['5', 'Five']],
+		valueField        :  'code',
+		displayField      :  'desc',
+		width             :  150,
+		height            :  150,
+		allowBlank        :  true
+	});
+	
+	this.add(multiselect);
+	this.addButton('Get Value', function() { alert(formMultiselect.getForm().getValues(true)); })
+	this.addButton('Set Value (2,3)', function() { multiselect.setValue('2,3'); })
+	this.addButton('Mark Invalid', function() { multiselect.markInvalid('Invalid'); })
+	this.addButton('Toggle Enabled', function() { if(!multiselect.disabled) {multiselect.disable();} else {multiselect.enable();} });
+	this.addButton('Reset', function() { multiselect.reset();});
+
+//	alert('before render form-ct-multiselect');
+//	this.render('form-ct-multiselect','GoalInContext');
+	alert('4');
+}
+Ext.extend(uwm.property.ComboboxMult, Ext.form.FormPanel);
+
+
+/*
+uwm.property.ComboboxMult.prototype.render = function(container, position) {
+	uwm.property.ComboboxMult.superclass.render.apply(this, arguments);
+	
+	if (this.toolTipText) {
+		this.toolTip = new Ext.ToolTip( {
+		    target : container,
+		    html : this.toolTipText
+		});
+	}
+}
+*/
