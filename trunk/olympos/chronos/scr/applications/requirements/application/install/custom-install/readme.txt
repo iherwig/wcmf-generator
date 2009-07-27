da23c221578e4fa7dec1445a6f516bffbc7d606a
iherwig <iherwig@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2009-07-27 21:24:39 +0000

    use common mechanism for custom database scripts for installation and upgrade

---
+++ trunk/olympos/chronos/scr/applications/requirements/application/install/custom-install/readme.txt
@@ -0,0 +1,11 @@
+SQL scripts in this directory will be executed after
+execution of dbupdate.php. Each line of a script is supposed to
+be one SQL command.
+
+The script name must comply with the following naming convention:
+
+initSectionName + '.sql'
+
+where 'initSectionName' is the name of a configuration
+section that defines the database connection to be used with this
+script, e.g. 'database.sql'.
--- trunk/olympos/chronos/scr/applications/requirements/application/install/dbupdate.php
+++ trunk/olympos/chronos/scr/applications/requirements/application/install/dbupdate.php
@@ -22,6 +22,8 @@ error_reporting(E_ERROR | E_PARSE);
 require_once(BASE."wcmf/lib/util/class.Message.php");
 require_once(BASE."wcmf/lib/output/class.LogOutputStrategy.php");
 require_once(BASE."wcmf/lib/util/class.InifileParser.php");
+require_once(BASE."wcmf/lib/util/class.FileUtil.php");
+require_once(BASE."wcmf/lib/util/class.DBUtil.php");
 require_once(BASE."wcmf/lib/persistence/class.PersistenceFacade.php");
 require_once(BASE."wcmf/lib/util/class.Log.php");
 
@@ -117,7 +119,14 @@ foreach ($tables as $tableDef)
   $connection->CompleteTrans();
 }
 
-include('upgrade-cwm.php');
+// execute custom scripts from the directory 'custom-dbupdate'
+$sqlScripts = FileUtil::getFiles('custom-dbupdate', '/\.sql$/', true);
+foreach ($sqlScripts as $script)
+{
+  // extract the initSection from the filename
+  $initSection = basename($script, ".sql");
+  DBUtil::executeScript($script, $initSection);
+}
 
 Log::info("done.", "dbupdate");
 
