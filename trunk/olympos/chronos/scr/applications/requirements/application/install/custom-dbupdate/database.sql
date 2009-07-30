c42d90cb78f4999e7704a773a61f024be01b9f7f
iherwig <iherwig@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2009-07-30 12:07:30 +0000

    regenerated wcmf package with official fk_name hack, improved custom upgrade script

--- trunk/olympos/chronos/scr/applications/requirements/application/install/custom-dbupdate/database.sql
+++ trunk/olympos/chronos/scr/applications/requirements/application/install/custom-dbupdate/database.sql
@@ -1,4 +1,4 @@
 -- upgrade pre 0.9.5 to 0.9.5
-UPDATE `nm_user_role` SET `fk_user_id` = `fk_userrdb_id`;
-UPDATE `nm_user_role` SET `fk_role_id` = `fk_rolerdb_id`;
+UPDATE `nm_user_role` SET `fk_user_id` = `fk_userrdb_id` WHERE (`fk_userrdb_id` <> 0 AND NOT `fk_userrdb_id` IS NULL);
+UPDATE `nm_user_role` SET `fk_role_id` = `fk_rolerdb_id` WHERE (`fk_rolerdb_id` <> 0 AND NOT `fk_rolerdb_id` IS NULL);
 ALTER TABLE `nm_user_role` DROP PRIMARY KEY, ADD PRIMARY KEY(`fk_user_id`, `fk_role_id`);
