3336de68d9c444143e61e364a469d910e13df660
enikao <enikao@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2009-08-05 17:17:53 +0000

    Fixed ActionKey relation Controller -- Controller

--- trunk/olympos/chronos/scr/applications/requirements/gui/js/cwm/relations/NMChiControllerActionKeyChiController.class.js
+++ trunk/olympos/chronos/scr/applications/requirements/gui/js/cwm/relations/NMChiControllerActionKeyChiController.class.js
@@ -16,3 +16,29 @@ cwm.relations.NMChiControllerActionKeyChiController = function(modelNodeClass) {
 }
 
 Ext.extend(cwm.relations.NMChiControllerActionKeyChiController, uwm.model.EditableRelation);
+
+cwm.relations.NMChiControllerActionKeyChiController.prototype.initByDisplayResult = function(node) {
+	cwm.relations.NMChiControllerActionKeyChiController.superclass.initByDisplayResult.call(this, node);
+	this.data.action = node.values[1].action;
+	this.data.config = node.values[1].config;
+	this.data.context = node.values[1].context;
+	this.data.Name = node.values[1].Name;
+	this.data.Notes = node.values[1].Notes;
+	this.data.created = node.values[1].created;
+	this.data.creator = node.values[1].creator;
+	this.data.last_editor = node.values[1].last_editor;
+	this.data.modified = node.values[1].modified;
+}
+
+cwm.relations.NMChiControllerActionKeyChiController.prototype.populatePropertyForm = function(form) {
+	var realForm = form.getForm();
+	realForm.findField("action").loadValue(this.data.action);
+	realForm.findField("config").loadValue(this.data.config);
+	realForm.findField("context").loadValue(this.data.context);
+	realForm.findField("Name").loadValue(this.data.Name);
+	realForm.findField("Notes").loadValue(this.data.Notes);
+	realForm.findField("created").loadValue(this.data.created);
+	realForm.findField("creator").loadValue(this.data.creator);
+	realForm.findField("last_editor").loadValue(this.data.last_editor);
+	realForm.findField("modified").loadValue(this.data.modified);
+}
