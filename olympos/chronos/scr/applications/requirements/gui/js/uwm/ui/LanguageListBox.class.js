87eb872f4db29f8b048d5d22d62eaf2b1e0fe846
Ingo Herwig <iherwig@users.noreply.github.com>
2009-08-11 14:59:00 +0000

    added translation panel to property container (open with tool in the property title bar, no functionality yet, another icon would be nice)

--- olympos/chronos/scr/applications/requirements/gui/js/uwm/ui/LanguageListBox.class.js
+++ olympos/chronos/scr/applications/requirements/gui/js/uwm/ui/LanguageListBox.class.js
@@ -20,32 +20,49 @@ Ext.namespace("uwm.ui");
  *            config The configuration object.
  */
 uwm.ui.LanguageListBox = Ext.extend(Ext.form.ComboBox, {
-    initComponent:function() {
-	
-		var data = uwm.Config.languages;
- 
-		Ext.apply(this, {
-			fieldLabel: uwm.Dict.translate('Language'),
-			forceSelection: 'true',
-			editable: 'false',
-			name: 'Language',
-			store: new Ext.data.ArrayStore({
-				id: 0,
-				fields: [
-					'key',
-					'val'
-				],
-				data: data
-			}),
-			valueField: 'key',
-			displayField: 'val',
-			mode: 'local',
-			editable: false,
-			triggerAction: 'all'
-		});
-	
-        uwm.ui.LanguageListBox.superclass.initComponent.call(this);
- 
-		this.setValue(data[0][0]);
-    }
-});
+		/**
+		 * @cfg {Boolean} includeDefault
+		 * 
+		 * Indicates if the default language should be included or not.
+		 * Defaults to true.
+		 */
+		includeDefault: true,
+		
+		initComponent:function() {
+		
+			var data = uwm.Config.languages;
+			
+			// remove the default language if requested
+			if (!this.includeDefault) {
+				for (var i=0; i<data.length; i++) {
+					if (data[i][0] == uwm.Config.defaultLanguage) {
+						data.splice(i, 1);
+					}
+				}
+			}
+			
+			Ext.apply(this, {
+				fieldLabel: uwm.Dict.translate('Language'),
+				forceSelection: 'true',
+				editable: 'false',
+				name: 'Language',
+				store: new Ext.data.ArrayStore({
+					id: 0,
+					fields: [
+						'key',
+						'val'
+					],
+					data: data
+				}),
+				valueField: 'key',
+				displayField: 'val',
+				mode: 'local',
+				editable: false,
+				triggerAction: 'all'
+			});
+		
+			uwm.ui.LanguageListBox.superclass.initComponent.call(this);
+			this.setValue(data[0][0]);
+		}
+	}
+);
