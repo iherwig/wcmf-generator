4881e33ea38d791e55f39933c0b8b91fc4b7fb05
enikao <enikao@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2009-08-13 12:09:39 +0000

    Added ReadTest

---
+++ trunk/olympos/dionysos/TestDionysusJson/src/net/sourceforge/olympos/dionysus/json/test/base/ReadTest.groovy
@@ -0,0 +1,162 @@
+package net.sourceforge.olympos.dionysus.json.test.base;
+
+import static org.junit.Assert.*
+
+import org.junit.Test;
+import net.sourceforge.olympos.dionysus.json.test.DionysusTest;
+import net.sourceforge.olympos.dionysus.json.test.Cfg;
+public class ReadTest extends DionysusTest {
+	public ReadTest(String method) {
+		super(method)
+	}
+	
+	@Test
+	public void simple() {
+		ensureLogin()
+
+		request(
+			[
+			 	action: 'read',
+			 	oid: Cfg.readBaseOid
+			],
+			{req, json ->
+				assertTrue(json.success)
+				assertEquals('read', json.action)
+				assertEquals(Cfg.readBaseOid, json.oid)
+				
+				assertObject(json.object, Cfg.readBaseOid, false)
+				
+				def firstLevel = json.object.attributes[Cfg.readBaseAttributeName]
+				assertObject(firstLevel, Cfg.readFirstLevelOid, false)
+
+				def firstLevelParent = firstLevel.attributes[Cfg.readFirstLevelParentAttributeName]
+                assertObject(firstLevelParent, Cfg.readBaseOid, true)
+
+				def secondLevel = firstLevel.attributes[Cfg.readFirstLevelAttributeName]
+                assertObject(secondLevel, Cfg.readSecondLevelOid, true)
+			},
+			this.method
+		)
+	}
+
+	@Test
+	public void depth0() {
+		ensureLogin()
+
+		request(
+			[
+			 	action: 'read',
+			 	oid: Cfg.readBaseOid,
+			 	depth: 0
+			],
+			{req, json ->
+				assertTrue(json.success)
+				assertEquals('read', json.action)
+				assertEquals(Cfg.readBaseOid, json.oid)
+				assertEquals(0, json.depth)
+				
+				assertObject(json.object, Cfg.readBaseOid, false)
+				
+				def firstLevel = json.object.attributes[Cfg.readBaseAttributeName]
+				assertObject(firstLevel, Cfg.readFirstLevelOid, true)
+			},
+			this.method
+		)
+	}
+
+	@Test
+	public void depth2() {
+		ensureLogin()
+
+		request(
+			[
+			 	action: 'read',
+			 	oid: Cfg.readBaseOid,
+			 	depth: 2
+			],
+			{req, json ->
+				assertTrue(json.success)
+				assertEquals('read', json.action)
+				assertEquals(Cfg.readBaseOid, json.oid)
+				assertEquals(2, json.depth)
+				
+				assertObject(json.object, Cfg.readBaseOid, false)
+				
+				def firstLevel = json.object.attributes[Cfg.readBaseAttributeName]
+				assertObject(firstLevel, Cfg.readFirstLevelOid, false)
+			
+				def firstLevelParent = firstLevel.attributes[Cfg.readFirstLevelParentAttributeName]
+			    assertObject(firstLevelParent, Cfg.readBaseOid, true)
+			
+				def secondLevel = firstLevel.attributes[Cfg.readFirstLevelAttributeName]
+			    assertObject(secondLevel, Cfg.readSecondLevelOid, false)
+			},
+			this.method
+		)
+	}
+
+	@Test
+	public void depthUnlimited() {
+		ensureLogin()
+
+		request(
+			[
+			 	action: 'read',
+			 	oid: Cfg.readBaseOid,
+			 	depth: -1
+			],
+			{req, json ->
+				assertTrue(json.success)
+				assertEquals('read', json.action)
+				assertEquals(Cfg.readBaseOid, json.oid)
+				assertEquals(-1, json.depth)
+				
+				assertObject(json.object, Cfg.readBaseOid, false)
+				
+				def firstLevel = json.object.attributes[Cfg.readBaseAttributeName]
+				assertObject(firstLevel, Cfg.readFirstLevelOid, false)
+			
+				def firstLevelParent = firstLevel.attributes[Cfg.readFirstLevelParentAttributeName]
+			    assertObject(firstLevelParent, Cfg.readBaseOid, true)
+			
+				def secondLevel = firstLevel.attributes[Cfg.readFirstLevelAttributeName]
+			    assertObject(secondLevel, Cfg.readSecondLevelOid, false)
+			},
+			this.method
+		)
+	}
+	
+	@Test
+	public void depthInvalid() {
+		ensureLogin()
+
+		request(
+			[
+			 	action: 'read',
+			 	oid: Cfg.readBaseOid,
+			 	depth: -2
+			],
+			{req, json ->
+				assertFalse(json.success)
+				assertEquals('read', json.action)
+				assertEquals(Cfg.readBaseOid, json.oid)
+				assertEquals(-2, json.depth)
+				
+				assertEquals('DEPTH_INVALID', json.errorCode)
+			},
+			this.method
+		)
+	}
+
+	private void assertObject(json, oid, boolean isReference) {
+		assertNotNull(json)
+		assertEquals(oid, json.oid)
+		assertEquals(getClassNameFromOid(oid), json.className)
+		assertNotNull(json.lastChange)
+		assertEquals(isReference, json.isReference)
+	}
+	
+	private String getClassNameFromOid(String oid) {
+		return oid.split(':')[0]
+	}
+}
