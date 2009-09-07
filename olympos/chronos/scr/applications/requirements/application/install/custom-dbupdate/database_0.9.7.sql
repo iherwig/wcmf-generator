2894c00ba2defb3ed8b4932e4d6c48a48701914e
Ingo Herwig <iherwig@users.noreply.github.com>
2009-09-07 12:26:44 +0000

    fixed language table case in update script, updated initial data sql

--- olympos/chronos/scr/applications/requirements/application/install/custom-dbupdate/database_0.9.7.sql
+++ olympos/chronos/scr/applications/requirements/application/install/custom-dbupdate/database_0.9.7.sql
@@ -1,2 +1,2 @@
-INSERT INTO `language` (`id`, `code`, `name`, `notes`, `created`, `creator`, `last_editor`, `modified`) VALUES(1, 'en', 'English', NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `language` (`id`, `code`, `name`, `notes`, `created`, `creator`, `last_editor`, `modified`) VALUES(2, 'de', 'Deutsch', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `Language` (`id`, `code`, `name`, `notes`, `created`, `creator`, `last_editor`, `modified`) VALUES(1, 'en', 'English', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `Language` (`id`, `code`, `name`, `notes`, `created`, `creator`, `last_editor`, `modified`) VALUES(2, 'de', 'Deutsch', NULL, NULL, NULL, NULL, NULL);
