20fe49f1c98ecd3e01aaa17fbcc79f4e8f590ef7
Ingo Herwig <iherwig@users.noreply.github.com>
2009-09-07 11:31:16 +0000

    changed install/update scripts to support different independent sql scripts for the same database (others are still executed if one failed)

--- olympos/chronos/scr/applications/requirements/application/install/custom-dbupdate/database.sql
+++
@@ -1,8 +0,0 @@
--- upgrade pre 0.9.5 to 0.9.5
-UPDATE `nm_user_role` SET `fk_user_id` = `fk_userrdb_id` WHERE (`fk_userrdb_id` <> 0 AND NOT `fk_userrdb_id` IS NULL);
-UPDATE `nm_user_role` SET `fk_role_id` = `fk_rolerdb_id` WHERE (`fk_rolerdb_id` <> 0 AND NOT `fk_rolerdb_id` IS NULL);
-ALTER TABLE `nm_user_role` DROP PRIMARY KEY, ADD PRIMARY KEY(`fk_user_id`, `fk_role_id`);
-
--- upgrade 0.9.5 to 0.9.7
-INSERT INTO `language` (`id`, `code`, `name`, `notes`, `created`, `creator`, `last_editor`, `modified`) VALUES(1, 'en', 'English', NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `language` (`id`, `code`, `name`, `notes`, `created`, `creator`, `last_editor`, `modified`) VALUES(2, 'de', 'Deutsch', NULL, NULL, NULL, NULL, NULL);
--- olympos/chronos/scr/applications/requirements/application/install/custom-dbupdate/readme.txt
+++ olympos/chronos/scr/applications/requirements/application/install/custom-dbupdate/readme.txt
@@ -4,8 +4,8 @@ be one SQL command.
 
 The script name must comply with the following naming convention:
 
-initSectionName + '.sql'
+initSectionName + '_' + suffix + '.sql'
 
 where 'initSectionName' is the name of a configuration
 section that defines the database connection to be used with this
-script, e.g. 'database.sql'.
+script and suffix is any string, e.g. 'database_1.sql'.
