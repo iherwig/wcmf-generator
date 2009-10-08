ef75e26fb2437a973173ce47d8d6f3ce54292917
pmrobinsonuk <pmrobinsonuk@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2009-10-08 09:28:07 +0000

    ExecuteActionSetTest

--- olympos/dionysos/TestDionysosJson/src/net/sourceforge/olympos/dionysos/json/test/base/ExecuteActionSetTest.groovy
+++ olympos/dionysos/TestDionysosJson/src/net/sourceforge/olympos/dionysos/json/test/base/ExecuteActionSetTest.groovy
@@ -11,53 +11,91 @@ public class ExecuteActionSetTest extends DionysosTest {
 	public ExecuteActionSetTest(String method) {
 		super(method)
 	}
-	//Need to check this is proper test case or not
+	
+	//Simple two actions, not related
 	@Test
 	public void simple() {
 		ensureLogin()
-
+		
 		request(
-			[
-			 	action: 'executeActionSet',
-			 	actionSet: [
-			 	            	action0: [
-			 	            	          		action: 'create',
-			 	            	          		className: Cfg.executeActionSetcreateClassName
-
-											],
-				 	            action1: [
-				 	                      		action: 'update',
-				 	                      		oid: Cfg.executeActionSetUpdateOid,
-				 	                      		attributes:  Cfg.executeActionSetAttributes
-
-										]
-             
-
-						]	
-			 	 
-			],
-			{req, json ->
-				assertTrue(json.success)
-				assertEquals('executeActionSet', json.action)
-				assertEquals('create',json.actionSet.action0.action)
-				assertEquals(Cfg.executeActionSetcreateClassName,json.actionSet.action0.className)
-				assertEquals('update',json.actionSet.action1.action)
-				 
+				[
+				action: 'executeActionSet',
+				actionSet: [
+				            action0: [
+				                      action: 'create',
+				                      className: Cfg.executeActionSetcreateClassName
+				                      ],
+				            action1: [
+				                      action: 'create',
+				                      className: Cfg.executeActionSetcreateClassName
+				                      ]
+				]	
 				
-				assertTrue(json.resultSet.action0.success)
-				assertEquals('create',json.resultSet.action0.action)
-				assertEquals(Cfg.executeActionSetcreateClassName,json.resultSet.action0.className)
-				assertNotNull(json.resultSet.action0.oid)
-
-				assertTrue(json.resultSet.action1.success)
-				assertEquals(json.resultSet.action0.oid,json.resultSet.action1.oid)
-				//assertEquals(Cfg.executeActionSetAttributes,json.resultSet.action1.attributes)
-				assertEquals('update',json.resultSet.action1.action)
-				 
-				
-				
-			},
-			this.method
-		)
+				],
+				{req, json ->
+					assertTrue(json.success)
+					assertEquals('executeActionSet', json.action)
+					assertEquals('create',json.actionSet.action0.action)
+					assertEquals(Cfg.executeActionSetcreateClassName,json.actionSet.action0.className)
+					assertEquals('create',json.actionSet.action1.action)
+					
+					assertTrue(json.resultSet.action0.success)
+					assertEquals('create',json.resultSet.action0.action)
+					assertEquals(Cfg.executeActionSetcreateClassName,json.resultSet.action0.className)
+					assertNotNull(json.resultSet.action0.oid)
+					
+					assertTrue(json.resultSet.action1.success)
+					//assertEquals(json.resultSet.action0.oid,json.resultSet.action1.oid)
+					//assertEquals(Cfg.executeActionSetAttributes,json.resultSet.action1.attributes)
+					assertEquals('create',json.resultSet.action1.action)
+						
+				},
+				this.method
+				)
 	}
+
+	//Simple two actions, not related
+	@Test
+	public void createAndUpdateWithOidParam() {
+		ensureLogin()
+		
+		request(
+				[
+				action: 'executeActionSet',
+				actionSet: [
+				            action0: [
+				                      action: 'create',
+				                      className: Cfg.executeActionSetcreateClassName
+				                      ],
+				            action1: [
+				                      action: 'update',
+				                      oid: Cfg.executeActionSetUpdateOid,
+				                      attributes: Cfg.executeActionSetAttributes
+				                      ]
+				]	
+				
+				],
+				{req, json ->
+					assertTrue(json.success)
+					assertEquals('executeActionSet', json.action)
+					assertEquals('create',json.actionSet.action0.action)
+					assertEquals(Cfg.executeActionSetcreateClassName,json.actionSet.action0.className)
+					assertEquals('create',json.actionSet.action1.action)
+					
+					assertTrue(json.resultSet.action0.success)
+					assertEquals('create',json.resultSet.action0.action)
+					assertEquals(Cfg.executeActionSetcreateClassName,json.resultSet.action0.className)
+					assertNotNull(json.resultSet.action0.oid)
+					
+					assertTrue(json.resultSet.action1.success)
+					assertEquals(json.resultSet.action0.oid,json.resultSet.action1.oid)
+					assertEquals(Cfg.executeActionSetAttributes,json.resultSet.action1.attributes)
+					assertEquals('update',json.resultSet.action1.action)
+						
+				},
+				this.method
+				)
+	}
+	
+
 }
