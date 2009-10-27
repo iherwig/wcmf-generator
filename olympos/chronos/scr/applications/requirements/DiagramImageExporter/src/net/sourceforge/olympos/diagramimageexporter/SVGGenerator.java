518db50e4bb4ac57be1224ba721b41362b24263d
Ingo Herwig <iherwig@users.noreply.github.com>
2009-10-27 15:33:29 +0000

    added method for external call

--- olympos/chronos/scr/applications/requirements/DiagramImageExporter/src/net/sourceforge/olympos/diagramimageexporter/SVGGenerator.java
+++ olympos/chronos/scr/applications/requirements/DiagramImageExporter/src/net/sourceforge/olympos/diagramimageexporter/SVGGenerator.java
@@ -1,11 +1,10 @@
 package net.sourceforge.olympos.diagramimageexporter;
 
 import java.util.ArrayList;
-import org.jdom.JDOMException;
 
 public class SVGGenerator {
 
-	// Arraylist for the diagram and Figures
+	// Array list for the diagram and Figures
 	public static ArrayList<InfoXmlDiagram> diagram;
 	private static ArrayList<InfoXmlFigure> xmlFigure;
 	private static ArrayList<InfoConnectionExist> connectionExist;
@@ -34,29 +33,35 @@ public class SVGGenerator {
 		xmlFigure.add(fChild);
 	}
 
-	public static void main(String[] args) throws JDOMException, Exception {
+	public static void generateImages(String sourceFile, String targetDir, String iconDir) throws Exception {
+
 		diagram = new ArrayList<InfoXmlDiagram>();
 		xmlFigure = new ArrayList<InfoXmlFigure>();
 		connectionExist = new ArrayList<InfoConnectionExist>();
 
 		SVGGenerator svg = new SVGGenerator();
 
-		String xmlFile = args[0];
-		String imagePath = args[1];
-		String picturePath = args[2];
-		
-		ElementDiagram.initCatalog(picturePath);
+		ElementDiagram.initCatalog(iconDir);
 
 		XmlReader xml = new XmlReader();
-		xml.XML(xmlFile);
+		xml.XML(sourceFile);
 
 		Draw df = new Draw();
 		ArrayList<InfoXmlDiagram> xmlDia = svg.getDiagram();
 		for (InfoXmlDiagram dia1 : xmlDia) {
 			ArrayList<InfoFigureParameter> figureArray = dia1.getFigure();
-			df.drawAll(imagePath, figureArray, dia1.getId());
+			df.drawAll(targetDir, figureArray, dia1.getId());
 		}
 
 		System.out.println("FINISH");
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		String xmlFile = args[0];
+		String imagePath = args[1];
+		String picturePath = args[2];
+
+		generateImages(xmlFile, imagePath, picturePath);
 	}
 }
