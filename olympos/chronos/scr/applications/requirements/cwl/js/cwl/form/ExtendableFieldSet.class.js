63801b39e0b3d81bd58560dc173e1eb66a02fd6d
Ingo Herwig <iherwig@users.noreply.github.com>
2009-11-18 19:25:40 +0000

    added add and delete functionality for text fields in field sets

---
+++ olympos/chronos/scr/applications/requirements/cwl/js/cwl/form/ExtendableFieldSet.class.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2008 The Olympos Development Team.
+ *
+ * http://sourceforge.net/projects/olympos/
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html. If redistributing this code,
+ * this entire header must remain intact.
+ */
+Ext.namespace("cwl.form");
+
+/**
+ * @class A field set with the possibility to dynamically add/remove fields.
+ * 
+ * @extends Ext.form.FieldSet
+ * @constructor
+ * @param {Object} config Configuraton of this field set.
+ */
+cwl.form.ExtendableFieldSet = function(config) {
+	var self = this;
+
+	var newBtn = new cwl.form.AddField({
+		fieldSet: this
+	});
+	
+	cwl.form.ExtendableFieldSet.superclass.constructor.call(this, Ext.apply(this, {
+		checkboxToggle: false,
+		collapsible: false,
+		autoHeight: true,
+		defaults: {width: 500},
+		defaultType: 'textfield',
+		collapsed: false,
+		items: [{
+						name: 'field1',
+						allowBlank: false
+				},
+				newBtn
+		]
+	}, config));
+}
+
+Ext.extend(cwl.form.ExtendableFieldSet, Ext.form.FieldSet);
