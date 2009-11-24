4ff1ac32ff1cc260cf1ff7a8cdd8acbd098b5760
iherwig <iherwig@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2009-11-24 16:08:22 +0000

    updated revision number

--- trunk/olympos/chronos/scr/applications/requirements/gui/js/uwm/Constants.js
+++ trunk/olympos/chronos/scr/applications/requirements/gui/js/uwm/Constants.js
@@ -22,7 +22,7 @@ uwm.Constants.DD_GROUP = "uwmDDGroup";
  *
  * @type String
  */
-uwm.Constants.SVN_REVISION ="1310";
+uwm.Constants.SVN_REVISION ="1360";
 
 /**
  * Timeout of AJAX requests, in milliseconds.
