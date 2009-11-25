4afb4e43c8b1a13fd36c7045e96313d9437cebb5
Niko Stotz <enikao@users.noreply.github.com>
2009-11-25 17:35:20 +0000

    Added simple counter for template generation

---
+++ olympos/chronos/scr/generator/src/net/sourceforge/olympos/oaw/extend/Counter.java
@@ -0,0 +1,29 @@
+package net.sourceforge.olympos.oaw.extend;
+
+import java.util.HashMap;
+
+public class Counter {
+	private static HashMap<String, Integer> counters = new HashMap<String, Integer>();
+
+	public static int incrementCounter(String counterId) {
+		int counterValue = getValue(counterId);
+
+		counterValue++;
+
+		counters.put(counterId, counterValue);
+
+		return counterValue;
+	}
+
+	public static int getValue(String counterId) {
+		int result = 0;
+
+		if (counters.containsKey(counterId)) {
+			result = counters.get(counterId);
+		} else {
+			counters.put(counterId, 0);
+		}
+
+		return result;
+	}
+}
