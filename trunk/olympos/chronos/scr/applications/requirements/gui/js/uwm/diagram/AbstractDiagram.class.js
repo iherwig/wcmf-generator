bf07a968908001d7a2863cab3cd167285813753c
iherwig <iherwig@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2010-03-17 16:36:14 +0000

    ignore figures with invalid geometry (position and size) (fixes bug #2971237)

--- trunk/olympos/chronos/scr/applications/requirements/gui/js/uwm/diagram/AbstractDiagram.class.js
+++ trunk/olympos/chronos/scr/applications/requirements/gui/js/uwm/diagram/AbstractDiagram.class.js
@@ -361,7 +361,7 @@ uwm.diagram.AbstractDiagram.prototype.handleLoaded = function() {
 			for ( var j in parentOids) {
 				var parentOid = parentOids[j];
 				
-				if (!(parentOid instanceof Function) && parentOid != this.getOid() && figure instanceof uwm.diagram.Figure) {
+				if (!(parentOid instanceof Function) && parentOid != this.getOid() && figure instanceof uwm.diagram.Figure && figure.hasValidGeometry()) {
 					this.figures.add(parentOid, figure);
 					modelContainer.loadByOid(parentOid, this.actionSet, 2);
 				}
