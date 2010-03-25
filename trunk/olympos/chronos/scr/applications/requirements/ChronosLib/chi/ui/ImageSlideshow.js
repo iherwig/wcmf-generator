92bb6a0636bbccfd2380daf0e39cd918da2dc1b6
iherwig <iherwig@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2010-03-25 14:02:15 +0000

    added random and image width parameters to slideshow

--- trunk/olympos/chronos/scr/applications/requirements/ChronosLib/chi/ui/ImageSlideshow.js
+++ trunk/olympos/chronos/scr/applications/requirements/ChronosLib/chi/ui/ImageSlideshow.js
@@ -12,19 +12,44 @@
 Ext.namespace("chi.ui");
 
 /**
- * @class A slideshow component. After configuring the image store, the slideshow is started 
- * using the start method. An alternative way of setting up the store manually is using the
+ * @class A slideshow component. After configuring the slideshow and image store, the slideshow 
+ * is started using the start method. An alternative way to manual setup is using the
  * static chi.ui.ImageSlideshow.createFromConfig method, which returns a slideshow instance.
  * Based on code from http://www.extjs.com/forum/showthread.php?t=36229
+ *
+ * Two alternatives exist for image positioning. To change the behaviour the stylesheet has
+ * to be modified according to the definitions below.
+ * 
+ * 1. center all images, larger images will resize the container
+ * 
+ * .slideshow {
+ * 	display: table-cell;
+ * 	vertical-align: middle;
+ * 	text-align: center;
+ * }
+ * 
+ * 2. align all images to the upper left corner, crop larger images to the container size
+ * 
+ * .slideshow {
+ * 	overflow: hidden;
+ * 	position: relative;
+ * }
+ * .slideshow img {
+ * 	display:block;
+ * 	position:absolute;
+ * 	left:0;
+ * 	top:0;
+ * }
  * 
  * @extends Ext.Container
  * @constructor
  * @param {Object} config The configuration object.
- * @config {Integer} transitionTime The time in seconds used for fading images
- * @config {String} easing The easing effect used in transitions (a valid Ext.lib.Easing value)
- * @config {Integer} imageDuration The default time in milliseconds each image is displayed
+ * @config {Integer} transitionTime The time in seconds used for fading images [default: 1]
+ * @config {String} easing The easing effect used in transitions (a valid Ext.lib.Easing value) [default: easeNone]
+ * @config {Boolean} random True/False wether images should be displayed in random order or not [default: false]
+ * @config {Integer} imageDuration The default time in milliseconds each image is displayed [default: 5000]
  * @config {Ext.data.Store} store The data store that describes the images to display (optional, see url)
- *   Each record has the values 'url' and 'duration'
+ *   Each record has the values 'url', 'duration' and optionally 'width'
  * @config url The url to load the store records from (ignored, if store is given)
  */
 chi.ui.ImageSlideshow = Ext.extend(Ext.Container, {
@@ -32,23 +57,19 @@ chi.ui.ImageSlideshow = Ext.extend(Ext.Container, {
 	constructor: function(config) {
 		this.transitionTime = config.transitionTime || 1;
 		this.easing = config.easing || 'easeNone';
+		this.random = config.random || false;
 		this.imageDuration = config.imageDuration || 5000;
 		
-		// this css class is necessary for centering the images
-		/*
-		.slideshow {
-			display: table-cell;
-			vertical-align: middle;
-			text-align: center;
-		}
-		*/
+		// this css class is necessary for image positioning
 		this.baseCls = 'slideshow';
 
 		this.imageIndex = 0;
 		this.timeoutId = undefined;
 
-		this.tpl = new Ext.Template('<img src="{url}"');
-		this.tpl.compile();
+		this.unsizedTpl = new Ext.Template('<img src="{url}">');
+		this.unsizedTpl.compile();
+		this.widthTpl = new Ext.Template('<img src="{url}" style="width:{width}px">');
+		this.widthTpl.compile();
 
 		if (config.store) {
 				this.store = config.store;
@@ -60,7 +81,8 @@ chi.ui.ImageSlideshow = Ext.extend(Ext.Container, {
 				root: 'data',
 				fields: [
 					{name: 'url', type: 'string'},
-					{name: 'duration', type: 'int'}
+					{name: 'duration', type: 'int'},
+					{name: 'width', type: 'int'}
 				]
 			});
 		} else {
@@ -86,6 +108,9 @@ chi.ui.ImageSlideshow = Ext.extend(Ext.Container, {
 	 * Start the slideshow.
 	 */
 	start: function() {
+		if (this.random) {
+			this.shuffleRecords();
+		}
 		this.preload();
 	},
 		
@@ -117,7 +142,7 @@ chi.ui.ImageSlideshow = Ext.extend(Ext.Container, {
 			var r = this.store.getAt(this.imageIndex);
 			
 			// use individual image duration, if defined
-			var pause = r.get('duration') || this.pause;
+			var duration = r.get('duration') || this.imageDuration;
 			
 			// fade out the old image
 			this.el.fadeOut({
@@ -129,41 +154,69 @@ chi.ui.ImageSlideshow = Ext.extend(Ext.Container, {
 				callback: function() {
 					
 					// fade in the new image
-					this.tpl.overwrite(this.el.dom, r.data);
+					if (r.get('width')) {
+						this.widthTpl.overwrite(this.el.dom, r.data);
+					}
+					else {
+						this.unsizedTpl.overwrite(this.el.dom, r.data);
+					}
 					this.el.fadeIn({
+						endOpacity: 0.9999,
 						duration: this.transitionTime,
 						easing: this.easing,
 						scope: this,
 						callback: function() {
 
-						// prevent multiple calls to this callback
+							// prevent multiple calls to this callback
 							if (this.timeoutId == undefined) {
 								this.imageIndex++;
 								if( this.imageIndex == this.store.getCount() ) {
 									this.imageIndex = 0;
 								}
 								// recall the loop method after the given display duration
-								this.timeoutId = this.loop.defer(pause, this);
+								this.timeoutId = this.loop.defer(duration, this);
 							}
 						}
 					});
 				}
 			});
 		}
+	},
+
+	/**
+	 * Shuffle the records contained in the store.
+	 */
+	shuffleRecords: function() {
+		var records = this.store.getRange();
+		var tmp, rand;
+		for(var i=0; i<records.length; i++) {
+			rand = Math.floor(Math.random() * records.length);
+			tmp = records[i]; 
+			records[i] = records[rand]; 
+			records[rand] =tmp;
+		}
+		// replace records with shuffled records
+		this.store.suspendEvents(false);
+		this.store.removeAll();
+		this.store.add(records);
+		this.store.resumeEvents();
 	}
 });
 
 /**
  * Create a slideshow instance from a configuration.
- * @param {Object} config A configuration object with properties width, height, images (see below)
+ * @param {Object} config A configuration object with properties width, height, random, 
+ *   imageDuration, images (array of arrays with entries for url, duration, width)
  * @return {chi.ui.ImageSlideshow} A slideshow instance
  *
  * A configuration example:
  * wmm.Slides.CREATE_PROJECT = {
- * 	width: 900,
- * 	height: 700,
+ * 	width: 800,
+ * 	height: 450,
+ * 	random: false,
+ * 	imageDuration: 1000,
  * 	images: [
- * 		['img/slides/img1.jpg', 1000],
+ * 		['img/slides/img1.jpg', 1000, 800],
  * 		['img/slides/img2.jpg', 4000]
  * 	]
  * };
@@ -172,13 +225,16 @@ chi.ui.ImageSlideshow.createFromConfig = function(config) {
 	var store = new Ext.data.SimpleStore({
 		fields: [
 			{name: 'url', type: 'string'},
-			{name: 'duration', type: 'int'}
+			{name: 'duration', type: 'int'},
+			{name: 'width', type: 'int'}
 		]
 	});
 	var slideshow = new chi.ui.ImageSlideshow({
 		store: store,
 		width: config.width,
-		height: config.height
+		height: config.height,
+		random: config.random,
+		imageDuration: config.imageDuration
 	});
 	store.loadData(config.images);
 	return slideshow;
