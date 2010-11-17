ceada26ed49b29ffab3cc8b2873c1572007fffe2
iherwig <iherwig@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2010-11-17 08:46:53 +0000

    fixed uml export (missing diagramFormat parameter)

--- trunk/olympos/chronos/scr/applications/requirements/gui/js/uwm/modeltree/ModelNode.class.js
+++ trunk/olympos/chronos/scr/applications/requirements/gui/js/uwm/modeltree/ModelNode.class.js
@@ -70,7 +70,7 @@ uwm.modeltree.ModelNode.prototype.buildContextMenu = function() {
 			    new uwm.ui.LongTaskRunner( {
 			        title : uwm.Dict.translate('Exporting UML ...'),
 			        call : function(successHandler, errorHandler) {
-				        uwm.persistency.Persistency.getInstance().exportUwm(self.getModelNode().getOid(), userLanguage, successHandler, errorHandler);
+				        uwm.persistency.Persistency.getInstance().exportUwm(self.getModelNode().getOid(), userLanguage, 'normal', successHandler, errorHandler);
 			        },
 			        successHandler : function(data) {},
 			        errorHandler : function(data) {
