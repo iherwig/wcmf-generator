f4ceb9f83db217170f5972a468ecbe64b3f6e560
jlaczay <jlaczay@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2011-02-21 17:11:55 +0000

    Updated release number for release 0.9.9.5

--- olympos/chronos/scr/applications/requirements/gui/js/uwm/Constants.js
+++ olympos/chronos/scr/applications/requirements/gui/js/uwm/Constants.js
@@ -22,7 +22,7 @@ uwm.Constants.DD_GROUP = "uwmDDGroup";
  *
  * @type String
  */
-uwm.Constants.SVN_REVISION ="1598";
+uwm.Constants.SVN_REVISION ="1921";
 
 /**
  * Timeout of AJAX requests, in milliseconds.
