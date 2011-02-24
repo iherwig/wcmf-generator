5985cc4c3d14c4c8ac692dc09d0390afb49acfa2
tschuetz <tschuetz@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2011-02-24 15:40:09 +0000

    updated revsion for build

--- olympos/chronos/scr/applications/requirements/gui/js/uwm/Constants.js
+++ olympos/chronos/scr/applications/requirements/gui/js/uwm/Constants.js
@@ -1,12 +1,16 @@
 /*
- * Copyright (c) 2008 The Olympos Development Team. *  * http://sourceforge.net/projects/olympos/
+ * Copyright (c) 2008 The Olympos Development Team.
+ * 
+ * http://sourceforge.net/projects/olympos/
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html. If redistributing this code,
  * this entire header must remain intact.
- */Ext.namespace("uwm.Constants");
+ */
+
+Ext.namespace("uwm.Constants");
 
 /**
  * Name of the ExtJS Drag&amp;Drop group for Model Objects.
@@ -22,7 +26,7 @@ uwm.Constants.DD_GROUP = "uwmDDGroup";
  *
  * @type String
  */
-uwm.Constants.SVN_REVISION ="1921";
+uwm.Constants.SVN_REVISION ="1938";
 
 /**
  * Timeout of AJAX requests, in milliseconds.
