e6d54e4846bbee2509d2804ad2179d2f8ad41eaa
jlaczay <jlaczay@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2011-03-31 16:29:01 +0000

    Added SQL statement to remove unused tables NMUserRole, UserRDB, Adodbseq

--- olympos/chronos/scr/applications/requirements/application/install/custom-dbupdate/database_0.9.9.6.sql
+++ olympos/chronos/scr/applications/requirements/application/install/custom-dbupdate/database_0.9.9.6.sql
@@ -1,2 +1,5 @@
 # Update legacy mappings without reference_type set
 UPDATE ChiValueRef SET ChiValueRef.reference_type=CONCAT('ChiNode:', (SELECT ChiValue.fk_chinode_id FROM ChiValue WHERE ChiValue.id = SUBSTR(ChiValueRef.reference_value, 10))) WHERE ChiValueRef.reference_type IS NULL;
+
+# Remove unused tables
+DROP TABLE IF EXISTS NMUserRole, UserRDB, RoleRDB, Adodbseq;
