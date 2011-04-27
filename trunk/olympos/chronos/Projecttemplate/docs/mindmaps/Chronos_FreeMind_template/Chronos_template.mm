4d6d13c06afe55b678084e7b10f7831bcea7b5c8
pgiuseppe <pgiuseppe@b914c4c3-1445-0410-9855-fd43ed2fdb6f>
2011-04-27 16:49:28 +0000

    Chronos templates in FreeMind and OO Calc format.

---
+++ trunk/olympos/chronos/Projecttemplate/docs/mindmaps/Chronos_FreeMind_template/Chronos_template.mm
@@ -0,0 +1,332 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<attribute_registry>
+<attribute_name NAME="Fea. Status" RESTRICTED="true">
+<attribute_value VALUE=""/>
+<attribute_value VALUE="Implemented "/>
+<attribute_value VALUE="Proposed "/>
+<attribute_value VALUE="Rejected "/>
+<attribute_value VALUE="Validated "/>
+</attribute_name>
+<attribute_name NAME="Req. Status" RESTRICTED="true">
+<attribute_value VALUE=""/>
+<attribute_value VALUE="Mandatory"/>
+<attribute_value VALUE="Proposed"/>
+<attribute_value VALUE="Satisfied"/>
+<attribute_value VALUE="Validated"/>
+</attribute_name>
+<attribute_name NAME="Type" RESTRICTED="true">
+<attribute_value VALUE=""/>
+<attribute_value VALUE="ChiBusinessUseCase"/>
+<attribute_value VALUE="ChiFeature"/>
+<attribute_value VALUE="ChiGoal"/>
+<attribute_value VALUE="ChiIssue"/>
+<attribute_value VALUE="ChiRequirement"/>
+</attribute_name>
+</attribute_registry>
+<node CREATED="1228144317281" ID="ID_1823873164" MODIFIED="1228149784890" TEXT="Chronos Requirements">
+<font NAME="SansSerif" SIZE="12"/>
+<hook NAME="accessories/plugins/HierarchicalIcons.properties"/>
+<attribute_layout NAME_WIDTH="132" VALUE_WIDTH="141"/>
+<node CREATED="1228146996359" FOLDED="true" HGAP="29" ID="ID_392158259" MODIFIED="1228149842203" POSITION="right" VSHIFT="-47">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Toolbox </b>
+    </p>
+    <p>
+      Copy this nodes in your Mind Map
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="desktop_new"/>
+<attribute_layout NAME_WIDTH="132" VALUE_WIDTH="141"/>
+<node CREATED="1228144960531" HGAP="28" ID="ID_694416638" MODIFIED="1228149596406" VSHIFT="-24">
+<richcontent TYPE="NODE"><html>
+  <head>
+    <style type="text/css">
+      <!--
+        p { margin-top: 0 }
+      -->
+    </style>
+    
+  </head>
+  <body>
+    <p>
+      <img src="ChiGoal.png"/>
+      Ziel
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="ChiGoal"/>
+<attribute NAME="Priority" VALUE="50"/>
+<attribute NAME="Value Name" VALUE=""/>
+<attribute NAME="Value ammount" VALUE=""/>
+<attribute NAME="Value Goal" VALUE=""/>
+</node>
+<node CREATED="1228144432312" ID="ID_499951676" MODIFIED="1228149160203">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="ChiRequirement.png"/>
+      Anforderung
+    </p>
+  </body>
+</html></richcontent>
+<arrowlink DESTINATION="ID_499951676" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1326168600" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="ChiRequirement"/>
+<attribute NAME="Priority" VALUE="50"/>
+<attribute NAME="Author" VALUE=""/>
+<attribute NAME="Proofreader" VALUE=""/>
+<attribute NAME="Req. Status" VALUE="Proposed"/>
+<attribute NAME="Req. Type" VALUE=""/>
+</node>
+<node CREATED="1228144362937" HGAP="43" ID="ID_463174273" MODIFIED="1228149555781" VSHIFT="21">
+<richcontent TYPE="NODE"><html>
+  <head>
+    <style type="text/css">
+      <!--
+        body { font-family: SansSerif; font-size: 12pt; background-color: #ffffff }
+        p { margin-top: 0 }
+      -->
+    </style>
+    
+  </head>
+  <body>
+    <p>
+      <img src="ChiIssue.png"/>
+      Offene Punkt
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="ChiIssue"/>
+<attribute NAME="Author" VALUE=""/>
+<attribute NAME="Responsible" VALUE=""/>
+</node>
+<node CREATED="1228144657578" HGAP="37" ID="ID_1130108599" MODIFIED="1228149577515" VSHIFT="18">
+<richcontent TYPE="NODE"><html>
+  <head>
+    <style type="text/css">
+      <!--
+        p { margin-top: 0 }
+      -->
+    </style>
+    
+  </head>
+  <body>
+    <p>
+      <img src="ChiFeature.png"/>
+      Massnahme
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="ChiFeature"/>
+<attribute NAME="Author" VALUE=""/>
+<attribute NAME="Proofreader" VALUE=""/>
+<attribute NAME="Fea. Status" VALUE="Proposed "/>
+</node>
+<node CREATED="1228148660484" HGAP="41" ID="ID_1135208032" MODIFIED="1228149079812" VSHIFT="29">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="ChiBusinessUseCase.png"/>
+      Anwendungsfall
+    </p>
+  </body>
+</html></richcontent>
+<attribute NAME="Type" VALUE="ChiBusinessUseCase"/>
+<attribute NAME="Author" VALUE=""/>
+<attribute NAME="Primary Actor" VALUE=""/>
+<attribute NAME="Goal in Context" VALUE=""/>
+<attribute NAME="Scope" VALUE=""/>
+<attribute NAME="Level" VALUE=""/>
+<attribute NAME="Stakeholders" VALUE=""/>
+<attribute NAME="Precondition" VALUE=""/>
+<attribute NAME="Main Success Scenario" VALUE=""/>
+<attribute NAME="Extensions" VALUE=""/>
+</node>
+</node>
+<node CREATED="1228147792296" HGAP="23" ID="ID_215450736" MODIFIED="1303922706430" POSITION="right" TEXT="Mind Map structure example" VSHIFT="33">
+<font NAME="SansSerif" SIZE="12"/>
+<attribute_layout NAME_WIDTH="132" VALUE_WIDTH="141"/>
+<node CREATED="1228144960531" HGAP="97" ID="ID_451956133" MODIFIED="1228149662000" VSHIFT="-4">
+<richcontent TYPE="NODE"><html>
+  <head>
+    <style type="text/css">
+      <!--
+        p { margin-top: 0 }
+      -->
+    </style>
+    
+  </head>
+  <body>
+    <p>
+      <img src="ChiGoal.png"/>
+      mein Ziel
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<attribute_layout NAME_WIDTH="109"/>
+<attribute NAME="Type" VALUE="ChiGoal"/>
+<attribute NAME="Priority" VALUE="50"/>
+<attribute NAME="Value Name" VALUE="earning"/>
+<attribute NAME="Value ammount" VALUE="20%"/>
+<attribute NAME="Value Goal" VALUE="120"/>
+<node CREATED="1228144432312" HGAP="56" ID="ID_787646724" MODIFIED="1228148095734" VSHIFT="3">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="ChiRequirement.png"/>
+      mein Szenario
+    </p>
+  </body>
+</html></richcontent>
+<arrowlink DESTINATION="ID_787646724" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1836549912" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="ChiRequirement"/>
+<attribute NAME="Priority" VALUE="50"/>
+<attribute NAME="Author" VALUE="domain expert"/>
+<attribute NAME="Proofreader" VALUE="Manager"/>
+<attribute NAME="Req. Status" VALUE="Proposed"/>
+<attribute NAME="Req. Type" VALUE="Scenario"/>
+<node CREATED="1228144432312" HGAP="33" ID="ID_1663276132" MODIFIED="1228149130484" VSHIFT="-47">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="ChiRequirement.png"/>
+      Anforderung 1
+    </p>
+  </body>
+</html></richcontent>
+<arrowlink DESTINATION="ID_1663276132" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Freemind_Arrow_Link_1971964578" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="ChiRequirement"/>
+<attribute NAME="Priority" VALUE="50"/>
+<attribute NAME="Author" VALUE="Analyst"/>
+<attribute NAME="Proofreader" VALUE="Manager"/>
+<attribute NAME="Req. Status" VALUE="Proposed"/>
+<attribute NAME="Req. Type" VALUE="Business Rule"/>
+<node CREATED="1228144362937" HGAP="83" ID="ID_426376886" MODIFIED="1228149645562" VSHIFT="-34">
+<richcontent TYPE="NODE"><html>
+  <head>
+    <style type="text/css">
+      <!--
+        p { margin-top: 0 }
+      -->
+    </style>
+    
+  </head>
+  <body>
+    <p>
+      <img src="ChiIssue.png"/>
+      mein Offene Punkt
+    </p>
+  </body>
+</html></richcontent>
+<attribute NAME="Type" VALUE="ChiIssue"/>
+<attribute NAME="Author" VALUE="domain expert"/>
+<attribute NAME="Responsible" VALUE="manager"/>
+</node>
+</node>
+<node CREATED="1228144432312" HGAP="52" ID="ID_1435927883" MODIFIED="1228149136406" VSHIFT="55">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="ChiRequirement.png"/>
+      Anforderung 2
+    </p>
+  </body>
+</html></richcontent>
+<arrowlink DESTINATION="ID_1435927883" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_36057733" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font NAME="SansSerif" SIZE="12"/>
+<attribute NAME="Type" VALUE="ChiRequirement"/>
+<attribute NAME="Priority" VALUE="50"/>
+<attribute NAME="Author" VALUE="ich"/>
+<attribute NAME="Proofreader" VALUE="Manager"/>
+<attribute NAME="Req. Status" VALUE="Proposed"/>
+<attribute NAME="Req. Type" VALUE="Persistence"/>
+<node CREATED="1228144657578" HGAP="65" ID="ID_1520303491" MODIFIED="1228149635234" VSHIFT="-4">
+<richcontent TYPE="NODE"><html>
+  <head>
+    <style type="text/css">
+      <!--
+        p { margin-top: 0 }
+      -->
+    </style>
+    
+  </head>
+  <body>
+    <p>
+      <img src="ChiFeature.png"/>
+      mein Massnahme
+    </p>
+  </body>
+</html></richcontent>
+<attribute NAME="Type" VALUE="ChiFeature"/>
+<attribute NAME="Author" VALUE="Analyst"/>
+<attribute NAME="Proofreader" VALUE="manager"/>
+<attribute NAME="Fea. Status" VALUE="Proposed "/>
+<node CREATED="1228148660484" HGAP="41" ID="ID_1914963321" MODIFIED="1228149688703" VSHIFT="29">
+<richcontent TYPE="NODE"><html>
+  <head>
+    <style type="text/css">
+      <!--
+        p { margin-top: 0 }
+      -->
+    </style>
+    
+  </head>
+  <body>
+    <p>
+      <img src="ChiBusinessUseCase.png"/>
+      My Anwendungsfall
+    </p>
+  </body>
+</html></richcontent>
+<attribute_layout NAME_WIDTH="95" VALUE_WIDTH="145"/>
+<attribute NAME="Type" VALUE="ChiBusinessUseCase"/>
+<attribute NAME="Author" VALUE="Analyst"/>
+<attribute NAME="Primary Actor" VALUE="Techniker"/>
+<attribute NAME="Goal in Context" VALUE=""/>
+<attribute NAME="Scope" VALUE="System A"/>
+<attribute NAME="Level" VALUE="Domain Obj a"/>
+<attribute NAME="Stakeholders" VALUE=""/>
+<attribute NAME="Precondition" VALUE=""/>
+<attribute NAME="Main Success Scenario" VALUE=""/>
+<attribute NAME="Extensions" VALUE=""/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1228149225031" HGAP="25" ID="ID_561583356" LINK="http://garr.dl.sourceforge.net/sourceforge/olympos/Chronos_Profile_Documentation.pdf" MODIFIED="1228149837437" POSITION="left" TEXT="Help" VSHIFT="-36">
+<icon BUILTIN="help"/>
+<attribute_layout NAME_WIDTH="132" VALUE_WIDTH="141"/>
+</node>
+</node>
+</map>
