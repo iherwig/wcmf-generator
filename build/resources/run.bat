f1ee8988cdf3c9d395358971e4b179501e1fcbf7
iherwig <ingo@wemove.com>
2023-06-28 21:18:29 +0000

    Some more cleanup

--- build/resources/run.bat
+++ build/resources/run.bat
@@ -1,1 +1,1 @@
-java -Djava.library.path=./lib/ -jar ChronosGenerator.jar workflow/cwm2uml.oaw -basePath=. -propertyFile=workflow/cwm2uml.properties
+java -Djava.library.path=./lib/ -jar ChronosGenerator.jar workflow/wcmf.oaw -basePath=. -propertyFile=workflow/wcmf.properties
--- build/run cwm.bat
+++
@@ -1,2 +0,0 @@
-java -Djava.library.path=./lib/ -jar wCMFGenerator.jar workflow/workflow.oaw -basePath=. -propertyFile=workflow/cwm.properties
-pause
